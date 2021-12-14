788c804c95c5100a0c00d2871d307d5655530f45
nitesh0707 <67994999+nitesh0707@users.noreply.github.com>
2021-12-14 16:54:44 +0530

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-alptnj*so3lin)4akjg$z=!_noqy-yu@ak6bme&q_txw=goz(-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
